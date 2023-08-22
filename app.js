b84232b3647f2f78b2780e0210d8920c8dbf1acb
Umesh Kumar <56982559+Umesh-JNU@users.noreply.github.com>
2023-08-22 12:49:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = "./config/config.env";
 // const path = "./config/local.env";
 
 dotenv.config({ path });
-
+console.log(process.env.NODE_ENV, "env");
 app.use(express.json());
 app.use(
   cors({
